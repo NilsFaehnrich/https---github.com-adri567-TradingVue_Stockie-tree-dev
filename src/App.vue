9a628b31db282142c6ee3349b3a2c23424d61382
Nils.Faehnrich <nils.faehnrich@stud.th-owl.de>
2021-12-14 14:19:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="header">
+      <Header/>
+    </div>
+    <div class="companybox">
+      <Company/>
+    </div>
+    <div class="chart">
+      <Chart/>
+    </div>
+    <div class="configbox">
+      <div class="timebox">
+        <ConfigBox/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from "./components/Chart.vue";
+import ConfigBox from "./components/ConfigBox.vue";
+import Company from "./components/Company.vue";
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Chart,
+    ConfigBox,
+    Company,
+    Header
+  },
+};
+</script>
+
+<style>
+/* Header */
+.header {
+  background-color: rgb(18, 24, 38);
+  width: 100%;
+  height: 8vh;
+  float: left;
+}
+
+/* Company */
+.companybox {
+  background-color: rgb(29, 39, 61);
+  width: 20%;
+  height: 92vh;
+  float: left;
+  padding: 30px 15px 30px 30px;
+  box-shadow: inset 0px 13px 3px -3px rgba(7, 16, 34, 0.15);
+}
+
+/* Chart */
+.chart {
+  background-color: rgb(29, 39, 61);
+  float: left;
+  width: 80%;
+  height: 77vh;
+  padding: 30px 30px 15px 30px;
+  box-shadow: inset 0px 13px 3px -3px rgba(7, 16, 34, 0.15);
+}
+
+/* Time */
+.configbox {
+  background-color: rgb(29, 39, 61);
+  float: left;
+  width: 80%;
+  height: 15vh;
+  padding: 15px 30px 30px 30px;
+}
+</style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,57 @@
+<template>  
+  <div class="chartview-sytle">
+    <div class="tradingchart">
+      <trading-vue :data="chart" :width="width" :height="height" title-txt="BTCUSD"></trading-vue>
+    </div>
+  </div>
+</template>
+
+<script>
+import TradingVue from "trading-vue-js";
+import Data from '../data/data.json'
+
+export default {
+  name: "TradingView",
+  components: { TradingVue },
+  methods: {
+    onResize() {
+      this.width = document.querySelector('.tradingchart').clientWidth;
+      this.height = document.querySelector('.tradingchart').clientHeight;
+    }
+  },
+  mounted() {
+      window.addEventListener('resize', this.onResize);
+      this.onResize();
+  },
+  beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+  },
+  data() {
+    return {
+      chart: Data,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.chartview-sytle {
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  padding: 10px;
+  background-color: rgb(18,24,38); 
+  border-radius: 25px;
+  padding: 30px;
+  box-shadow: 0 0 1em rgb(7, 16, 34)
+}
+
+.tradingchart {
+  height: 100%;
+  width: 100%;
+ background-color: rgb(18,24,38); 
+}
+</style>
